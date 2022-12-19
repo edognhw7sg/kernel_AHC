b0e033fc9bf267a3f3322aeb1c6051d45600ba39
Edo Seven-Sword/G <107063546+edognhw7sg@users.noreply.github.com>
2022-12-19 13:37:08 +0900

    Add files via upload

---
+++ kernel_AHC/__init__.py
@@ -0,0 +1,12 @@
+from kernel_AHC import HC_single_linkage
+from kernel_AHC import HC_complete_linkage
+from kernel_AHC import HC_average_linkage_between
+from kernel_AHC import HC_average_linkage_within
+from kernel_AHC import HC_centroid
+from kernel_AHC import HC_centroid_medoid
+from kernel_AHC import HC_ward
+from kernel_AHC import HC_ward_medoid
+from kernel_AHC import kernel_HC_dsim
+
+
+
